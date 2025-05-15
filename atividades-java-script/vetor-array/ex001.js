44118e548e12dd041395ff01b7604394fe5dd889
Mateus Silva <mateusdasilvavilaca@gmail.com>
2025-05-15 16:40:59 -0300

    exercício da faculdade

---
+++ atividades-java-script/vetor-array/ex001.js
@@ -0,0 +1,5 @@
+/* Escreva um algoritmo que solicite ao usuário 5 nomes e 5 notas e calcue a média das notas da turma (armazenar as notas em um array/vetor)
+*/
+
+var nomes = [window.alert(`Qual seu nome?`)]
+

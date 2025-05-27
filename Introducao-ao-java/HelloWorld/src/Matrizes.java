d915496f19bb12f4e025b27dfd7852067a19cfcc
Mateus Silva <mateusdasilvavilaca@gmail.com>
2025-05-27 15:08:41 -0300

    Vetores e Matrizes JAVA

---
+++ Introducao-ao-java/HelloWorld/src/Matrizes.java
@@ -0,0 +1,26 @@
+public class Matrizes {
+    public static void main (String [] args) {
+
+        int[][] matriz = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        System.out.println(matriz);
+
+        int [][] matrixTamFixo = new int [2][2];
+
+        // mostrar na tela
+
+        System.out.println("\nValores da matriz:");
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
